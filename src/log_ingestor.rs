4a6c94edeb6b0d32b1f9d6ee2b600d90949bcec0
namanlp <naman@garudalinux.org>
2023-11-19 19:50:45 +0530

    Done Log Reading

---
+++ src/log_ingestor.rs
@@ -0,0 +1,29 @@
+use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+
+use crate::log_entry_struct::LogEntry;
+
+
+// This
+pub async fn ingest() -> std::io::Result<()> {
+    // Here, we are reading the log entry from port 3000
+    // And pass it to Log Ingestor
+    HttpServer::new(|| { App::new()
+        // Take Logs from path /
+            .service(web::resource("/")
+                .route(web::post()
+                    .to(log_ingestor)))
+        // Bind logs to address localhost:3000
+    }).bind("127.0.0.1:3000")?
+        .run().await
+}
+
+async fn log_ingestor(data: web::Json<LogEntry>) -> impl Responder {
+
+    // Handling Log Entry Data
+    // Replace with Database Operation Later
+    println!("Received log entry: {:?}", data);
+
+    // If log was ingested successfully, return success message
+    HttpResponse::Ok().body("Log entry received successfully")
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(non_snake_case)]
+
+// Declaring Modules
+mod log_ingestor;
+mod log_entry_struct;
+mod query_interface;
+
+// Using Functions
+use futures::join;
+use log_ingestor::ingest;
+
+#[actix_web::main]
+async fn main(){
+
+    // Define Variable to run log ingestor
+    let log_ingestor_thread = ingest();
+    join!(log_ingestor_thread);
 }
