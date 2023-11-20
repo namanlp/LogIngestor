0ed9431cc2ab138131f7a94b140e233382eddb61
namanlp <naman@garudalinux.org>
2023-11-20 23:50:34 +0530

    Improve Readability

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,13 @@ mod log_entry_struct;
 mod query_interface;
 mod database_operations;
 
-// Using Functions
 use futures::join;
-use log_ingestor::ingest;
 
 #[actix_web::main]
 async fn main(){
 
     // Define Variable to run log ingestor
-    let log_ingestor_task = ingest();
+    let log_ingestor_task = log_ingestor::ingest();
 
     // Define Variable to run Query Interface
     let query_interface_task = query_interface::query_interface();
